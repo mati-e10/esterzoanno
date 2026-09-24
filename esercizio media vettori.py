e6836fa48d472660027785db2bfd3261f14a369c
mati-e10 <matilde.petturiti@campusdavinci.edu.it>
2026-09-24 10:37:26 +0200

    esercizio sulla media con i vettori

---
+++ esercizio media vettori.py
@@ -0,0 +1,13 @@
+"""
+data una lista [1,2,2,4,5,8,9] , calcolare e stampare a dideo la media
+"""
+listauno=[1,2,2,4,5,8,9]
+somma=0
+for i in range(0,7):
+    somma=somma+ listauno[i]
+    print("la somma è")
+    print(somma)
+media=somma/7
+print(media)
+
+    
