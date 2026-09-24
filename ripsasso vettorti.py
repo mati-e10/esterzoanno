862fa3b8a5f5d255086fdefaa4916739b3033f3d
mati-e10 <matilde.petturiti@campusdavinci.edu.it>
2026-09-24 10:37:55 +0200

    ripasso vettori

---
+++ ripsasso vettorti.py
@@ -0,0 +1,17 @@
+#creare una lista vuota#
+listauno=[]
+#creare una lista con elemnti
+listadue[1,2,3,4]
+#aggiungere un elemnto alla lista (in coda)
+listauno.append(4)
+#visulizzare lunghezza della lista
+print(len(listauno))
+#visualizzare gli elemnti di una lista
+print(listadue[0])
+print(listadue[1])
+print(listadue[2])
+print(listadue[3])
+
+
+for i in range(0,4):
+    print(listadue[i])
